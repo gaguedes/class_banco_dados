bc4a7a65464106cd3452359516f325528c34d20c
gaguedes <gguedes421@gmail.com>
2024-06-17 17:57:05 -0300

    Pequena refatoração

--- conexaoBancoDados.py
+++ conexaoBancoDados.py
@@ -1,23 +1,28 @@
 import pyodbc
 
 class dadosConexao:
-    def __init__(self, ipServidor,bancoDados,usuario,senha):
+    def __init__(self, ipServidor, bancoDados, usuario, senha):
         self.ipServidor = ipServidor
         self.bancoDados = bancoDados
         self.usuario = usuario
         self.senha = senha
 
 
-class acaoBancoDados(dadosConexao):
+class conexao(dadosConexao):
     def __init__(self, ipServidor, bancoDados, usuario, senha):
         dadosConexao.__init__(self, ipServidor, bancoDados, usuario, senha)
 
     def exec(self, query):
         self.query = query
-        
+
         cnxn = pyodbc.connect(
-            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.ipServidor + ';DATABASE=' + self.bancoDados + ';UID=' + self.usuario + ';PWD=' + self.senha + ';TrustServerCertificate=YES')
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + self.ipServidor + 
+            ';DATABASE=' + self.bancoDados + 
+            ';UID=' + self.usuario + 
+            ';PWD=' + self.senha + 
+            ';TrustServerCertificate=YES')
         cursor = cnxn.cursor()
+        
         try:
             if 'SELECT' in self.query and 'INSERT' not in self.query:
                 cursor.execute(self.query)
